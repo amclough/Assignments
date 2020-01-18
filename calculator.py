f7796b6b4c55f94dcc8fdc645aa6949a678739c4
ALINA CLOUGH <ALINACLOUGH@HKS.HARVARD.EDU>
2020-01-18 12:51:56 -0500

    tbh I am not great at math but I tried the bonus lol

---
+++ calculator.py
@@ -0,0 +1,32 @@
+#multiply a and b
+def multiply(a,b):
+    return a * b
+
+#divide a by b
+def divide(a,b):
+    return a / b
+
+#add a to b
+def add(a,b):
+    return a + b
+
+#subtract b from a
+def subtract(a,b):
+    return a - b
+
+print("I'm going use the calculator functions to multiply 5 and 6")
+x = multiply(5,6)
+print(x)
+
+#bonus
+#square a
+def square(a):
+    return a * a
+
+#cube a
+def cube (a):
+    return a * a *a
+
+#square_n_times
+def square_n_times(a, n):
+    return n(a * a)
